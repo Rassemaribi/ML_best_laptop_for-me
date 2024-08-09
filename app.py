5231ec94809fec4f18828c671c0aac6e1fdb84c4
Rassemaribi <Racem.Aribi@iit.ens.tn>
2024-08-09 15:40:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -96,6 +96,4 @@ def recommend(request: LaptopRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# Run the FastAPI app
